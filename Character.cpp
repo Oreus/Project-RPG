1170ebb89b5cb6a7fdbf99496669fcebdb48c13f
Oreus <Oreus14@outlook.com>
2025-05-29 01:00:43 -0600

    Updated Character.cpp for example of Polymorphism

--- Character.cpp
+++ Character.cpp
@@ -3,6 +3,9 @@
 Character::Character(string name, int level, int health, int MH, int attack, int defense, int speed)
     : name(name), level(level), health(health), maxHealth(MH), attackPower(attack), defense(defense), speed(speed) {}
 
+    void Character::setHealth(int h) {
+    health = h;
+}
     string Character::getName() {
     return name;
 }
