9982be273fd41b9e639bca00565555e5252460ce
Oreus <Oreus14@outlook.com>
2025-06-05 02:57:40 -0600

    Update RPG_Main.cpp to run the game
    Some corrections still need to be made in order to run monster names, weapon names, and armor names the way its supposed to.

--- RPG_Main.cpp
+++ RPG_Main.cpp
@@ -1,30 +1,42 @@
-#include "Character.h"
-#include "Monster.h"
-#include "Item.h"
-#include "Healing_Potion.h"
+#include "Armor.h"
 #include "Bomb.h"
+#include "Character.h"
+#include "Game.h"
+#include "Healing_Potion.h"
+#include "Inventory.h"
+#include "Item.h"
+#include "Monster.h"
+#include "Player.h"
+#include "Poison.h"
+#include "Shop.h"
+#include "StatusEffects.h"
+#include "Stun.h"
+#include "Weapon.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    Monster Dragon("Dragon", 13, 30, 28, 38, 13, 25, 13, 5.2);
-    cout << "Monster created!" << endl;
-    cout << "Name: " << Dragon.getName() << endl;
-    cout << "Level: " << Dragon.getLevel() << endl;
-    cout << "Health: " << Dragon.getHealth() << endl;
+    ///Monster Dragon("Dragon", 13, 30, 28, 38, 13, 25, 13, 5.2);
+    ///cout << "Monster created!" << endl;
+    ///cout << "Name: " << Dragon.getName() << endl;
+    ///cout << "Level: " << Dragon.getLevel() << endl;
+    ///cout << "Health: " << Dragon.getHealth() << endl;
 
-    Monster* enemy = new Monster("Goblin", 3, 40, 40, 15, 5, 10, 10, 1.5);
-    cout << "Initial Goblin health: " << enemy->getHealth() << endl;
+    ///Monster* enemy = new Monster("Goblin", 3, 40, 40, 15, 5, 10, 10, 1.5);
+    ///cout << "Initial Goblin health: " << enemy->getHealth() << endl;
     
-    Item* potion = new HealingPotion("Minor Potion", 15);
-    Item* bomb = new Bomb("Fire Bomb", 20);
+    ///Item* potion = new HealingPotion("Minor Potion", 15);
+    ///Item* bomb = new Bomb("Fire Bomb", 20);
 
-    potion->use(enemy);  
-    cout << "After using potion: " << enemy->getHealth() << endl;
+    ///potion->use(enemy);  
+    ///cout << "After using potion: " << enemy->getHealth() << endl;
 
-    bomb->use(enemy);
-    cout << "After using bomb: " << enemy->getHealth() << endl;
+    ///bomb->use(enemy);
+    ///cout << "After using bomb: " << enemy->getHealth() << endl;
+    ///return 0;
+
+    Game game;
+    game.start();
     return 0;
-
 };
