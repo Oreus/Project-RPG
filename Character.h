d6a409b0d7456e2db063046b5221c748098b44c5
Oreus <Oreus14@outlook.com>
2025-06-05 02:52:41 -0600

    Updated Character.h to be fully functional and to not need a Character.cpp anymore

--- Character.h
+++ Character.h
@@ -22,12 +22,19 @@ class Character{
     public:
         Character()
         : name(""), level(), health(), maxHealth(), attackPower(), defense(), speed(), statusEffects() {} 
-        Character(string name, int level, int health, int MH, int attack, int defense, int speed);
+        Character(string name, int level, int health, int MH, int attack, int defense, int speed)
+        : name(name), level(level), health(health), maxHealth(MH), attackPower(attack), defense(defense), speed(speed), statusEffects() {}
         
 
-        string getName();
-        int getLevel();
-        int getHealth();
+        string getName() {
+            return name;
+        }
+        int getLevel() {
+            return level;
+        }
+        int getHealth() {
+            return health;
+        }
         int getAttackPower() {
             return attackPower;
         }
@@ -38,20 +45,43 @@ class Character{
             return speed;
         }
 
-        void setLevel(int level);
-        void setHealth(int h);
-        void setAttackPower(int p);
-        void setDefense(int d);
-        void setSpeed(int s);
-
-        void attack(Character* target);
+        void setLevel(int l) {
+            level = l;
+        }
+        void setHealth(int h) {
+            health  =  h;
+        }
+        void setAttackPower(int p) {
+            attackPower = p;
+        }
+        void setDefense(int d) {
+            defense = d;
+        }
+        void setSpeed(int s) {
+            speed = s;
+        }
+        void attack(Character* target) {
+            target->takeDamage(attackPower);
+        }
         void takeDamage(int amount){
             health -= amount;
         }
         bool isAlive() {
             return health > 0;
         }  
-        void applyStatusEffects(StatusEffect* effect);
-        void processStatusEffects();
+        void applyStatusEffects(StatusEffect* effect) {
+            statusEffects.push_back(effect);
+        }
+        void processStatusEffects() {
+            for (int i = 0; i < statusEffects.size(); ++i) {
+            statusEffects[i]->applyEffect(this);
+            statusEffects[i]->update();
+            if (statusEffects[i]->isExpired()) {
+                statusEffects.erase(statusEffects.begin() + i);
+                i--;
+            }
+        }
+    }
 };
+
 #endif
