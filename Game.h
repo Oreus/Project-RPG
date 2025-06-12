6ca8b220a4a93ce0ecb204bed272a3195dee73ed
Oreus <Oreus14@outlook.com>
2025-06-12 00:58:58 -0600

    Updated Game.h to add documentation, and added exit game function along with function to validate user input

--- Game.h
+++ Game.h
@@ -1,27 +1,58 @@
 #ifndef GAME_H
 #define GAME_H
+#include <cstdlib>
 #include "Player.h"
 #include "Monster.h"
 #include "Shop.h"
 
 using namespace std;
 
+// Declaration of class Game
+// Handles the main game flow logic
 class Game {
     private:
+        // Instance variables
         Player* player;
         Monster* currentMonster;
         Shop* shop;
         int level;
         bool gameOver;
+        // A method for checking that user inputs match the options given
+        // Made in private to prevent any unwanted errors of calling in public
+        int promptForInt(const string& prompt) {
+            while (true) {
+                cout << prompt;
+                string input;
+                getline(cin, input);
+                if (input == "q" or input == "Q") {
+                    exitGame();
+                }
+                try {
+                    int value = stoi(input);
+                    return value;
+                }
+                catch (const exception& e) {
+                    cout << "Invalid input, please enter a valid number or 'q' to exit." << endl;
+                }
+            }
+        }
+        // Method for exiting the game without having to die
+        void exitGame() {
+            cout << "\nExiting game" << endl;
+            exit(0);
+        }
 
     public:
+        // Constructors, and makes a new shop on new game start
         Game() : player(nullptr), currentMonster(nullptr),
         shop(new Shop()), level(1), gameOver(false) {}
 
-        ~Game() {}
 
+        // Handles all of the "surface level" shop mechanics: buying, displaying, change equipment
         void handleShop() {
             cout << "\n--- Shop ---" << endl;
+            // Display player coins
+            cout << "You currenly have " << player->getCoins() << " coins." << endl;
             shop->displayItem();
             cout << "Enter the number of the item to buy (-1 to exit): ";
             int choice;
@@ -31,6 +62,8 @@ class Game {
                 if (!shop->buyItem(choice, player)) {
                     cout << "Invalid selection." << endl;
                 }
+                // Display updated coin count
+                cout << "You now have " << player->getCoins() << " coins." << endl;
                 cout << "Buy another item? (-1 to exit): ";
             }
             cout << "\nWould you like to change equipment? (1 = yes, 0 = no): ";
@@ -40,30 +73,34 @@ class Game {
                 player->changeEquipment();
             }
         }
+        // Handles the encounters between the player and monster
         void battleLoop() {
             cout << "\nA " << currentMonster->getName() << " appeared!" << endl;
             while (player->isAlive() && currentMonster->isAlive()) {
-                cout << "\nYour HP: " << player->getHealth() << " | Enemy HP: " << currentMonster->getHealth() << endl;
-                cout << "1) Attack  2) Use Item  3) Change Equipment\nChoose an action: ";
-                int action;
-                cin >> action;
+                cout << "\nYour HP: " << player->getHealth() 
+                << " | Enemy HP: " << currentMonster->getHealth() << endl;
+                int action = 
+                promptForInt("1) Attack  2) Use Item  3) Change Equipment\nChoose an action: ");
                 switch (action) {
+                    // Attack
                     case 1:
                         player->attack(currentMonster);
                         break;
+                    // Use item
                     case 2:
-                        player->getInventory()->listItems();
-                        cout << "Enter item index: ";
-                        int i;
-                        cin >> i;
-                        player->useItem(i);
+                        player->getInventory()->listItems(); {
+                        int itemIndex = promptForInt("Enter item index: ");
+                        player->useItem(itemIndex);
+                        }
                         break;
+                    // Change equipment
                 case 3:
                         player->changeEquipment();
                         break;
                     default:
                         cout << "Invalid action!" << endl;
                 }
+                // IF monster is alive, attack player
                 if (currentMonster->isAlive()) {
                     currentMonster->attack(player);
                 }
@@ -73,8 +110,10 @@ class Game {
                 cout << "\nYou defeated the " << currentMonster->getName() << "!" << endl;
             }
         }
+        // Generates a new monster with scaled level based on 
         Monster* generateMonster() {
             string name = "Monster Lvl" + to_string(level);
+            // Scales the level of the monster
             int hp = 50 + 10 * level;
             int attack = 10 + 2 * level;
             int def = 5 + level;
@@ -84,9 +123,11 @@ class Game {
 
             return new Monster(name, level, hp, hp, attack, def, spd, coins, difficulty);
         }
+        // Check to see if the player has died in order to end the game
         bool checkGameOver() {
             return !player->isAlive();
         }
+        // Controls the flow of the overall game such by combining the previous functions
          void gameLoop() {
             while (!gameOver) {
                 shop->updateInventory(level);
@@ -94,7 +135,7 @@ class Game {
                 currentMonster = generateMonster();
                 battleLoop();
                 gameOver = checkGameOver();
-
+                // Rewards the player for beating the current monster
                 if (!gameOver) {
                     player->addExperience(25* level);
                     player->addCoins(50 * level);
@@ -104,6 +145,7 @@ class Game {
             }
             cout << "\nGame Over." << endl;
         }
+        // The function that starts the main game loop
         void start() {
             cout << "Welcome to the beginning of your journey!" << endl;
             string name;
