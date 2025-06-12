33fbd30ede283a70c7fbcc8d9dedbc443f2abf4b
Oreus <Oreus14@outlook.com>
2025-06-12 01:02:57 -0600

    Updated Poison.h to add documentation

--- Poison.h
+++ Poison.h
@@ -5,25 +5,32 @@
 
 using namespace std;
 
+// Declaration of class Poison
+// Inherits from class StatusEffect
 class Poison : public StatusEffect{
     private:
+    // Amount of damage posion does per turn
         int damagePerTurn;
 
     public:
+        // Constructors
         Poison()
         : StatusEffect(), damagePerTurn() {}
         Poison(string name, int duration, int damagePerTurn);
-
+        // Applies posion effect
         void applyEffect(Character* target){
             if (target != nullptr) {
                 target->takeDamage(damagePerTurn);
             }
         }
+        // Update remaining turn duration
         void update(){
             duration--;
         }
+        // Checks to see if its expired
         bool isExpired(){
             return duration <= 0;
         }
 };
+
 #endif
