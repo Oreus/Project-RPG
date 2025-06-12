676ffb31a0cdfa6e5e74e406f66e998bfc729aee
Oreus <Oreus14@outlook.com>
2025-06-12 01:07:29 -0600

    Updated StatusEffects.h to add documentation

--- StatusEffects.h
+++ StatusEffects.h
@@ -7,29 +7,47 @@ using namespace std;
 
 class Character;
 
+// Declaration of class StatusEffect
+// This class is an abstract  class that is a base class 
+// for Posion and stun
 class StatusEffect{
     protected:
+        // Status Effect attributes
         string name;
         int duration;
     
     public:
+        // Constructors
         StatusEffect()
         : name(), duration() {}
         StatusEffect(string name, int duration)
         : name(name), duration(duration) {}
-
-        string getName();
-        int getDuration();
-        void setDuration(int d);
-
-        virtual void applyEffect(Character* target);
+        // Getters
+        string getName() {
+            return name;
+        }
+        int getDuration() {
+            return duration;
+        }
+        //Setter
+        void setDuration(int d) {
+            duration = d;
+        }
+        //Virtual method for the application of an effect
+        // is meant to be overriden by different effects
+        virtual void applyEffect(Character* target) = 0;
+        //Virtual method for the updating of an effect duration
+        // is meant to be overriden by different effects
         virtual void update() {
             if (duration > 0) {
-                duration = - 1;
+                duration--;
             }
         }
+        //Virtual method for the checking of an effect duration
+         // is meant to be overriden by different effects
         virtual bool isExpired() {
             return duration <= 0;
         }
 };
+
 #endif
