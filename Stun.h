ec1fd41df2b01433156319ea0263912c5180d37b
Oreus <Oreus14@outlook.com>
2025-06-12 01:07:55 -0600

    Updated Stun.h to add documentation

--- Stun.h
+++ Stun.h
@@ -1,23 +1,29 @@
 #ifndef STUN_H 
-#define POISOSTUN_HN_H
+#define STUN_H
 #include <string>
 #include "StatusEffects.h"
 #include "Character.h"
 
 using namespace std;
 
+// Declaration of class Stun
+// Inherits from class StatusEffects
 class Stun : public StatusEffect{
     public:
+        // Constructors
         Stun()
         : StatusEffect() {}
         Stun(string name, int duration);
-
+        // Applies stun effect
         void applyEffect(Character* target);
+        // Updates remaining turn duration
         void update() {
             duration--;
         }
+        // Checks to see if its expired
         bool isExpired() {
             return duration <= 0;
         }
 };
+
 #endif
