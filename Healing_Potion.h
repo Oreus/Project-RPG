2ebb6f3985158b6f683039b1614d178b8dc4a609
Oreus <Oreus14@outlook.com>
2025-06-12 00:59:49 -0600

    Updated Healing_Potion.h to add documentation

--- Healing_Potion.h
+++ Healing_Potion.h
@@ -6,17 +6,23 @@
 
 using namespace std;
 
+// Declaration of class HealingPotion
+// Handles the attribute and uses of Healing Potions
+// Inherits from class Item
 class HealingPotion : public Item {
     private:
+        // Amount of healing potion does
         int healingAmount;
     
     public:
+        // Constructors
         HealingPotion() 
         : Item(), healingAmount() {}
         HealingPotion(string name, int healingAmount)
         : Item(name), healingAmount(healingAmount) {}
-        
+        // Uses healing potion to restore health
         void use(Character* target) {
+            // Checks if target IS there
             if (target != nullptr) {
         int newHealth = target->getHealth() + healingAmount;
         target->setHealth(newHealth);
