83101dd3cff141465eb28a7ca8f17c75134f7d81
Oreus <Oreus14@outlook.com>
2025-06-12 00:56:59 -0600

    Updated Bomb.h to add in documentation and final touches

--- Bomb.h
+++ Bomb.h
@@ -6,16 +6,22 @@
 
 using namespace std;
 
+// Declaration of class Bomb
+// handles the attributes and uses of Bombs
+// Inherits from class Item
 class Bomb : public Item {
     private:
+        // Amount of damage bomb does
         int damageAmount;
 
     public:
+        // Comstructors
         Bomb()
         : Item(), damageAmount() {}
-        Bomb(string name, int damageAmount);
-
-    void use(Character* target) {
+        Bomb(string name, int damageAmount) 
+        : Item(name), damageAmount(damageAmount) {}
+        // Uses bomb
+        void use(Character* target) {
         if (target != nullptr) {
         target->takeDamage(damageAmount);
         }
