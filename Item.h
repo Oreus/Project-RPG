c6982f66492e6cbde04caeed89461d0f201e363b
Oreus <Oreus14@outlook.com>
2025-06-12 01:01:13 -0600

    Updated Item.h to add documentation

--- Item.h
+++ Item.h
@@ -5,22 +5,27 @@
 
 using namespace std;
 
+// Declaration of class Item
+// This class is an abstract class that is a base for
+// HealingPotion and Bomb
 class Item {
     private:
+        // Name of the item
         string name;
     
     public:
+    // Constructors
     Item()
     : name() {}
     Item(string name)
     : name(name) {}
-
+    //Getter
     string getName() {
         return name;
     }
-
-    virtual void use(Character* target){
-    }
+    // Virtual method for the use of the weapon
+    // is meant to be overriden by the items
+    virtual void use(Character* target) = 0;
 };
 
 #endif
